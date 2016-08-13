418bf26e76589e215dc21fb8751184ae057d7b10
King D Liu/WKS/Wistron <King_D_Liu@wistron.com>
2016-08-13 13:48:11 +0800

    import code performace.

---
+++ DemoTools/snowboy_demo/README.txt
@@ -0,0 +1,1 @@
+push all files in src/resources/ to the root path of Android internal storage.
--- DemoTools/snowboy_demo/src/main/java/com/wistron/demo/tool/snowboy_demo/MainActivity.java
+++ DemoTools/snowboy_demo/src/main/java/com/wistron/demo/tool/snowboy_demo/MainActivity.java
@@ -1,17 +1,29 @@
 package com.wistron.demo.tool.snowboy_demo;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import ai.kitt.snowboy.SnowboyDetect;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "King";
+
+    private static final String[] neededPermissions = new String[]{
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    private static final int REQUEST_CODE_ASK_PERMISSIONS = 1;
 
     private static final String AUDIO_RECORDER_TEMP_FILE = "record_temp.raw";
 
@@ -34,25 +46,53 @@ public class MainActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int i = 0;
+            for (; i < neededPermissions.length; i++) {
+                if (checkSelfPermission(neededPermissions[i]) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(neededPermissions, REQUEST_CODE_ASK_PERMISSIONS);
+                    break;
+                }
+            }
+            if (i >= neededPermissions.length) {
+                initial();
+            }
+        } else {
+            initial();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_ASK_PERMISSIONS) {
+            for (int i = 0; i < grantResults.length; i++) {
+                if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "You should agree all of the permissions, force exit! please retry", Toast.LENGTH_LONG).show();
+                    finish();
+                    return;
+                }
+            }
+            initial();
+        }
+    }
+
+    private void initial() {
         // Assume you put the model related files under /sdcard/snowboy/
-        snowboyDetector = new SnowboyDetect("/storage/emulated/legacy/common.res",
+        snowboyDetector = new SnowboyDetect(Environment.getExternalStorageDirectory().getAbsolutePath()+"/common.res",
                 /*"/storage/emulated/legacy/snowboy.umdl");*/
-                "/storage/emulated/legacy/hello_teddy.pmdl");
+                Environment.getExternalStorageDirectory().getAbsolutePath()+"/hello_teddy.pmdl");
         snowboyDetector.SetSensitivity("0.34");         // Sensitivity for each hotword
         snowboyDetector.SetAudioGain(1.0f);              // Audio gain for detection
         Log.i(TAG, "NumHotwords = "+snowboyDetector.NumHotwords()+", BitsPerSample = "+snowboyDetector.BitsPerSample()+", NumChannels = "+snowboyDetector.NumChannels()+", SampleRate = "+snowboyDetector.SampleRate());
 
-        initial(RECORDER_SAMPLERATE, RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING);
-    }
-
-    private void initial(int sampleRate, int channels, int audioEncoding) {
-        bufferSize = AudioRecord.getMinBufferSize
-                (sampleRate, channels, audioEncoding) * 3;
+        /*bufferSize = AudioRecord.getMinBufferSize
+                (sampleRate, channels, audioEncoding) * 3;*/
         bufferSize = snowboyDetector.NumChannels() * snowboyDetector.SampleRate() * 5;
         recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
-                sampleRate,
-                channels,
-                audioEncoding,
+                RECORDER_SAMPLERATE,
+                RECORDER_CHANNELS,
+                RECORDER_AUDIO_ENCODING,
                 bufferSize);
 
         startRecord();
